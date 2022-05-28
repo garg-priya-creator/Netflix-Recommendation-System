d1b09c9a5d5a00483a89481032225df0dc0b9ab6
garg-priya-creator <priyagarg072@gmail.com>
2022-05-28 23:48:51 +0530

    A web-app which can be used to get recommendations for a series/movie.
    A web-app which can be used to get recommendations for a series/movie, the app recommends a list of media according to list of entered choices of movies/series in your preferred language using Python and Flask for backend and HTML, CSS and JavaScript for frontend.
    
    #netflix-recommendation-system #netflix #recommendation-system #python #flask #html #css #countvectorizer #cosine-similarity #webapp

---
+++ app/README.md
@@ -0,0 +1,72 @@
+<h1 align="center">Netflix-Recommendation-System</h1>
+<p><font size="3">
+A web-app which can be used to get recommendations for a series/movie, the app recommends a list of media according to list of entered choices of movies/series in your preferred language using <strong>Python</strong> and <strong>Flask</strong> for backend and <strong>HTML</strong>, <strong>CSS</strong> and <strong>JavaScript</strong> for frontend.
+</p>
+
+##This web-app contains 3 main pages:
+-[Home Page](#home-page)
+-[Recommendation Page](#recommendation-page)
+-[Movie Detail Page](#movie-detail-page)
+-[Netflix Page](#netflix-page)
+
+#Home Page
+Here the user can choose list of their favourite movies and series and their preferred language. For example, I have entered a list with 2 Horror Movies(Insidious and Insidious Chapter 2), an action series(Supergirl) and a drama series(Suits) as my list of choices and English and Hindi as my preferred languages.
+Clicking on the Get Started button the user will see the list of recommendations.
+![](screenshots/Screenshot-HomePage.png)
+
+#Recommendation Page
+Here the user will get poster images of all the recommended movies and series sorted based upon their IMDb Scores.
+![](screenshots/Screenshot-RecommendationPage1.png)
+![](screenshots/Screenshot-RecommendationPage1.png)
+
+Clicking on any poster image, the user will be sent to the Movie Details page for the corresponding title.
+
+#Movie Detail Page
+Here are the complete details of the user selected title like Genre, Movie Summary, Languages in which movie is available, IMDb scores, Directors, Writers and Actors and so on. User will also find a link at the end of the page for the NEtflix Page of the corresponding title. 
+![](screenshots/Screenshot-MovieDetailPage1.png)
+![](screenshots/Screenshot-MovieDetailPage2.png)
+
+#Netflix Page
+This page is not a part of my web-app but an example what the user will see as the Netflix Page if they choose to click on the Netflix Link for the title.
+You can login into your Netflix account and enjoy watching your selected movie or series from our recommendations.
+![](screenshots/Screenshot-NetflixPage.png)
+
+# How To Use
+
+To be able to use this web app locally in a development environment you will need the following:
+
+1) You will need [Git](https://git-scm.com) installed on your computer.
+
+2) Then From your terminal, you should do the following:
+
+```cmd
+# Clone this repository
+git clone https://github.com/garg-priya-creator/Netflix-Recommendation-System.git
+
+# Go into the repository
+cd netflix-recommendation-system
+
+# Install flask (if you already haven't)
+pip install flask
+
+```
+3) To run this application you don't need to have any special configuration but make sure you don't change the directory of the project otherwise you can recieve errors while you try to run the app.
+
+4) You can run the Netflix React App using the following command from your terminal:
+
+```
+# Run the app
+>>set FLASK_APP=app.py
+>>flask run
+```
+
+# Author
+
+👤 **Priya Garg**
+- Github: https://github.com/garg-priya-creator
+- Linkedin: https://www.linkedin.com/in/priya-garg-9220381b3
+- Email: priyagarg072@gmail.com
+
+# Show Your Support
+
+Give a ⭐️ if you like this project!
---
+++ app/app.py
@@ -0,0 +1,87 @@
+import math
+from flask import Flask, render_template, request
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import sys
+
+def clean_data(x):
+        return str.lower(x.replace(" ", ""))
+
+def create_soup(x):
+    return x['Genre'] + ' ' + x['Tags'] + ' ' +x['Actors']+' '+ x['ViewerRating']
+
+def get_recommendations(title, cosine_sim):
+    global result
+    title=title.replace(' ','').lower()
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:51]
+    movie_indices = [i[0] for i in sim_scores]
+    result =  netflix_data.iloc[movie_indices]
+    result.reset_index(inplace = True)
+    return result
+
+netflix_data = pd.read_csv('NetflixDataset.csv',encoding='latin-1', index_col = 'Title')
+netflix_data.index = netflix_data.index.str.title()
+netflix_data = netflix_data[~netflix_data.index.duplicated()]
+netflix_data.rename(columns={'View Rating':'ViewerRating'}, inplace=True)
+Language = netflix_data.Languages.str.get_dummies(',')
+Lang = Language.columns.str.strip().values.tolist()
+Lang = set(Lang)
+Titles = set(netflix_data.index.to_list())
+
+netflix_data['Genre'] = netflix_data['Genre'].astype('str')
+netflix_data['Tags'] = netflix_data['Tags'].astype('str')
+netflix_data['IMDb Score'] = netflix_data['IMDb Score'].apply(lambda x: 6.6 if math.isnan(x) else x)
+netflix_data['Actors'] = netflix_data['Actors'].astype('str')
+netflix_data['ViewerRating'] = netflix_data['ViewerRating'].astype('str')
+new_features = ['Genre', 'Tags', 'Actors', 'ViewerRating']
+selected_data = netflix_data[new_features]
+selected_data.Genre = selected_data.Genre.apply(clean_data)
+selected_data.Tags = selected_data.Tags.apply(clean_data)
+selected_data.Actors = selected_data.Actors.apply(clean_data)
+selected_data.ViewerRating = selected_data.ViewerRating.apply(clean_data)
+selected_data.index = selected_data.index.str.lower()
+selected_data.index = selected_data.index.str.replace(" ",'')
+selected_data['soup'] = selected_data.apply(create_soup, axis = 1)
+
+count = CountVectorizer(stop_words='english')
+count_matrix = count.fit_transform(selected_data['soup'])
+cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
+selected_data.reset_index(inplace = True)
+indices = pd.Series(selected_data.index, index=selected_data['Title'])
+result = 0
+df = pd.DataFrame()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', languages = Lang, titles = Titles) 
+
+@app.route('/about',methods=['POST'])
+def getvalue():
+    global df
+    movienames = request.form.getlist('titles')
+    languages = request.form.getlist('languages')
+    for moviename in movienames:
+        get_recommendations(moviename,cosine_sim2)
+        for language in languages:
+            df = pd.concat([result[result['Languages'].str.count(language) > 0], df], ignore_index=True)
+    df.drop_duplicates(keep = 'first', inplace = True)
+    df.sort_values(by = 'IMDb Score', ascending = False, inplace = True)
+    images = df['Image'].tolist()
+    titles = df['Title'].tolist()
+    return render_template('result.html',  titles =  titles, images = images)
+
+@app.route('/moviepage/<name>')
+def movie_details(name):
+    global df
+    details_list = df[df['Title'] == name].to_numpy().tolist()
+    return render_template('moviepage.html', details = details_list[0])
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
